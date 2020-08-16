dad8d43c0e77b70115b761f9b16d13d03a7a15f1
Thomas Rainford <thomas@rainfords.net>
2020-08-16 14:17:55 +1200

    Add main components for todos

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoItem: React.FC = () => {
+    return (
+        <div className="TodoItem">
+
+        </div>
+    )
+}
+
+export default TodoItem
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { List } from '@material-ui/core'
+
+const TodoList: React.FC = () => {
+    return (
+        <div className="TodoList">
+            <List>
+                
+            </List>
+            
+        </div>
+    )
+}
+
+export default TodoList

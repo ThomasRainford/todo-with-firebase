a439176d70babbab3f4bcc7741dbe078054fc9b2
Thomas Rainford <thomas@rainfords.net>
2020-08-20 12:22:38 +1200

    Style add button.

--- src/components/NewTodoForm.tsx
+++ src/components/NewTodoForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles, Theme, TextField, Button } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
 import { Formik, Form } from 'formik'
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -31,7 +32,7 @@ const NewTodoFrom: React.FC<Props> = (props: Props) => {
                 {({ values, handleChange }) => (
                     <Form >
                         <TextField style={{width: '80%'}} placeholder="Add Todo" name="text" value={values.text} onChange={handleChange} />
-                        <Button type="submit">Add</Button>
+                        <Button size="small" startIcon={<SaveIcon/>} style={{marginLeft: '3%'}} variant="outlined" color="primary" type="submit">Add</Button>
                     </Form>
                 )}
             </Formik>

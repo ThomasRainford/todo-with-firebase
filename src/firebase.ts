66eeef7098d0937ac056711dc1de28602d97a2e6
Thomas Rainford <thomas@rainfords.net>
2020-09-02 15:35:11 +1200

    Create pull hook.

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,4 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase';
 import 'firebase/firestore';
 
 const config = {
@@ -16,4 +16,4 @@ const firebaseApp = firebase.initializeApp(config)
 
 const db = firebaseApp.firestore()
 
-export { db }
+export { db, firebase }
---
+++ src/hooks/useFirebaseFirestorePush.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { db, firebase } from '../firebase'
+
+const FirebaseDatabaseAPI = (): [
+    {
+        allTodos: firebase.firestore.DocumentData[] | undefined
+    },
+    Function
+] => {
+
+    const [allTodos, setAllTodos] = useState<firebase.firestore.DocumentData[]>()
+
+    const pullTodos = () => {
+        db.collection('todos').onSnapshot((snapshot) => {
+          setAllTodos(snapshot.docs.map((doc) => ({
+            id: doc.id,
+            todo: doc.data()
+          })))
+        })
+      }
+
+    return [
+        { allTodos },
+        pullTodos
+    ]
+}
+
+export default FirebaseDatabaseAPI

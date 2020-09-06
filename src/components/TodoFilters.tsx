f666436d9798ac2b81960adc95bec5aa1ac03b6f
Thomas Rainford <thomas@rainfords.net>
2020-09-06 12:17:24 +1200

    Refactor filtering so todos that are added are displayed.

--- src/components/TodoFilters.tsx
+++ src/components/TodoFilters.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, ReactNode } from 'react'
 import { makeStyles, Theme, Button, List } from '@material-ui/core'
 import TodoItem from './TodoItem'
 
@@ -17,6 +17,12 @@ const useStyles = makeStyles((theme: Theme) => ({
     }
 }))
 
+enum Filter {
+    All,
+    Active,
+    Complete
+}
+
 interface Props {
     todosFolder: firebase.firestore.DocumentData
 }
@@ -24,37 +30,45 @@ interface Props {
 const TodoFilters: React.FC<Props> = ({ todosFolder }) => {
     const classes = useStyles()
 
-    const allTodos = (): firebase.firestore.DocumentData[] => {
-        return todosFolder.todo.todos
+    const [filter, setFilter] = useState<Filter>(Filter.All)
+
+    const displayTodos = (): ReactNode => {
+
+        if (filter === Filter.All) {
+            return displayAll()
+
+        } else if (filter === Filter.Active) {
+            return displayActive()
+        }
+        
     }
 
-    const activeTodos = (): firebase.firestore.DocumentData[] => {
-        let index: number = 0
-        return todosFolder.todo.todos.filter(() => {
-            return !todosFolder.todo.completed[index++]
+    const displayAll = (): ReactNode => {
+        return todosFolder.todo.todos.map((todo: firebase.firestore.DocumentData) => {
+            return <TodoItem key={Math.random()} text={todo} currentTodos={todosFolder} index={todosFolder.todo.todos.indexOf(todo)} />
         })
     }
 
-    const completedTodos = (): firebase.firestore.DocumentData[] => {
+    const displayActive = (): ReactNode => {
         let index: number = 0
-        return todosFolder.todo.todos.filter(() => {
-            return todosFolder.todo.completed[index++]
+        return todosFolder.todo.todos.map((todo: firebase.firestore.DocumentData) => {
+            const complete = todosFolder.todo.completed[index++]
+            if(!complete) {
+                return <TodoItem key={Math.random()} text={todo} currentTodos={todosFolder} index={todosFolder.todo.todos.indexOf(todo)} />
+            }
+            
         })
     }
-
-    const [filteredTodos, setFilteredTodos] = useState<firebase.firestore.DocumentData[]>(allTodos())
 
     return (
         <div className={classes.TodoFilters}>
-            <Button onClick={() => { setFilteredTodos(allTodos()) }} className={classes.allButton} variant="contained" color="primary">All</Button>
-            <Button onClick={() => { setFilteredTodos(activeTodos()) }} className={classes.activeButton} variant="contained" color="primary">Active</Button>
-            <Button onClick={() => { setFilteredTodos(completedTodos()) }} className={classes.completedButton} variant="contained" color="primary">Completed</Button>
+            <Button onClick={() => { setFilter(Filter.All) }} className={classes.allButton} variant="contained" color="primary">All</Button>
+            <Button onClick={() => { setFilter(Filter.Active) }} className={classes.activeButton} variant="contained" color="primary">Active</Button>
+            <Button onClick={() => { setFilter(Filter.Complete) }} className={classes.completedButton} variant="contained" color="primary">Completed</Button>
 
             <List>
                 {todosFolder &&
-                    filteredTodos.map((todo: firebase.firestore.DocumentData) => (
-                        <TodoItem key={Math.random()} text={todo} currentTodos={todosFolder} index={todosFolder.todo.todos.indexOf(todo)} />
-                    ))
+                    displayTodos()
                 }
             </List>
         </div>

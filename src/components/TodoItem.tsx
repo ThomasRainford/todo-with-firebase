cb3f1cb3f8b29de2edf965ab909cd90cabfa6983
Thomas Rainford <thomas@rainfords.net>
2020-09-11 16:05:38 +1200

    Lighter todo divider.

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -90,7 +90,7 @@ const TodoItem: React.FC<Props> = ({ text, currentTodos, index }) => {
                     </IconButton>
                 </div>
             </ListItem>
-            <Divider />
+            <Divider light={true}/>
         </div>
     )
 }
